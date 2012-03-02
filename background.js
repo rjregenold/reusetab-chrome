89a6618e72dd7d65b9be0337b909c30bba9419c0
RJ Regenold <rjregenold@gmail.com>
2012-03-01 21:51:57 -0600

    initial commit.

---
+++ background.js
@@ -0,0 +1,15 @@
+function onTabCreated(newTab) {
+  var url = newTab.url;
+  var host = parseUri(url).host;
+  chrome.tabs.query({"pinned": true}, function(pinnedTabs) {
+    var matchedTab = _.find(pinnedTabs, function(pinnedTab) {
+      return parseUri(pinnedTab.url).host === host;
+    });
+    if (!matchedTab) return;
+    chrome.tabs.update(matchedTab.id, {"url": url, "active": true});
+    chrome.tabs.remove(newTab.id);
+  });
+}
+
+chrome.tabs.onCreated.addListener(onTabCreated);
+
